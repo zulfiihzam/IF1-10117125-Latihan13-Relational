4d12f7a6b6d50f24e3ff1d2636b4355d5c17ef8f
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:55:32 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan13-Relational/src/if1/pkg10117125/latihan13/relational/IF110117125Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan13.relational;
+
+/**
+ *
+ * @author
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF1
+ * NIM   : 10117125
+ * Deskripsi Program : berisi program yang 
+ * menampilkan hubungan itu benar atau salah
+ */
+public class IF110117125Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
